593f9cd0e66d0538ef6e5848eef71ba90c77ad91
Jorge <jorgenzcarrasco@gmail.com>
2022-06-18 15:08:47 -0500

    finish date class

---
+++ ConceptsPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 30));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
